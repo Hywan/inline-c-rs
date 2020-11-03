d4bf4e7b67d2621a73a10bf7576ee7dbb7a7980c
Ivan Enderlin <ivan@mnt.io>
2020-11-03 10:54:04 +0100

    chore: Clean up.

--- src/run.rs
+++ src/run.rs
@@ -2,11 +2,7 @@ use crate::assert::Assert;
 
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
-use std::env;
-use std::error::Error;
-use std::io::prelude::*;
-use std::process::Command;
+use std::{borrow::Cow, collections::HashMap, env, error::Error, io::prelude::*, process::Command};
 use tempfile::NamedTempFile;
 
 pub enum Language {
@@ -23,35 +19,8 @@ impl ToString for Language {
     }
 }
 
-const ENV_VAR_PREFIX: &'static str = "INLINE_C_RS_";
-
 pub fn run(language: Language, program: &str) -> Result<Assert, Box<dyn Error>> {
-    lazy_static! {
-        static ref REGEX: Regex =
-            Regex::new(r#"#inline_c_rs (?P<variable_name>[^:]+):\s*"(?P<variable_value>[^"]+)"\n"#)
-                .unwrap();
-    }
-
-    let mut variables = HashMap::new();
-
-    for (variable_name, variable_value) in env::vars().filter_map(|(mut name, value)| {
-        if name.starts_with(ENV_VAR_PREFIX) {
-            Some((name.split_off(ENV_VAR_PREFIX.len()), value))
-        } else {
-            None
-        }
-    }) {
-        variables.insert(variable_name, variable_value);
-    }
-
-    for captures in REGEX.captures_iter(program) {
-        variables.insert(
-            String::from(captures["variable_name"].trim()),
-            String::from(&captures["variable_value"]),
-        );
-    }
-
-    let program = &REGEX.replace_all(program, "");
+    let (program, variables) = collect_environment_variables(program);
 
     let mut program_file = NamedTempFile::new()?;
     program_file.write(program.as_bytes())?;
@@ -86,6 +55,39 @@ pub fn run(language: Language, program: &str) -> Result<Assert, Box<dyn Error>> 
     Ok(Assert::new(
         Command::new(object_path).envs(variables).output()?,
     ))
+}
+
+fn collect_environment_variables<'p>(program: &'p str) -> (Cow<'p, str>, HashMap<String, String>) {
+    const ENV_VAR_PREFIX: &'static str = "INLINE_C_RS_";
+
+    lazy_static! {
+        static ref REGEX: Regex =
+            Regex::new(r#"#inline_c_rs (?P<variable_name>[^:]+):\s*"(?P<variable_value>[^"]+)"\n"#)
+                .unwrap();
+    }
+
+    let mut variables = HashMap::new();
+
+    for (variable_name, variable_value) in env::vars().filter_map(|(mut name, value)| {
+        if name.starts_with(ENV_VAR_PREFIX) {
+            Some((name.split_off(ENV_VAR_PREFIX.len()), value))
+        } else {
+            None
+        }
+    }) {
+        variables.insert(variable_name, variable_value);
+    }
+
+    for captures in REGEX.captures_iter(program) {
+        variables.insert(
+            String::from(captures["variable_name"].trim()),
+            String::from(&captures["variable_value"]),
+        );
+    }
+
+    let program = REGEX.replace_all(program, "");
+
+    (program, variables)
 }
 
 fn add_compiler_flags(arguments: &mut Vec<String>, variables: &HashMap<String, String>) {
